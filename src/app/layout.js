bb8686b2c556899ff43e106ff0ba90fad384b523
Mihai Cujba <mihai.cujba@indrivo.com>
2024-01-14 17:40:06 +0200

    init

---
+++ src/app/layout.js
@@ -0,0 +1,26 @@
+import "./globals.css";
+
+import { Inter } from "next/font/google";
+import { PrismicPreview } from "@prismicio/next";
+import clsx from "clsx";
+
+import { repositoryName } from "@/prismicio";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+/**
+ * @param {{ children: React.ReactNode }}
+ */
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={clsx(inter.className)}>
+      <body className="overflow-x-hidden antialiased">
+        {children}
+        <PrismicPreview repositoryName={repositoryName} />
+      </body>
+    </html>
+  );
+}
